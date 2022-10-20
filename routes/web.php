32bb6c8651253352a1451e08cc077a06f6810e8d
Leonardo <contato@leowebdesigner.com.br>
2022-10-20 16:56:39 -0300

    route email

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test-email', function(){
    Mail::to('contato@leowebdesigner.com.br')
-   ->send(new TestMail);
+         ->send(new TestMail);
 
     return 'ok - mail sended';
 });
